6a92bdd6f95eda31db3cffb3c67a9a3722ceb457
vikasgaddam9010 <nxtwave@ip-10-2-37-137.ap-south-1.compute.internal>
2024-07-19 18:45:24 +0000

    Enhancement of Simple Todos

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -6,61 +6,125 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    isChecked: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    isChecked: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    isChecked: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    isChecked: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    isChecked: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    isChecked: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    isChecked: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    isChecked: false,
   },
 ]
 
 class SimpleTodos extends Component {
-  state = {todosList: initialTodosList}
+  state = {todosList: initialTodosList, newTodo: ''}
 
   onDeleteuser = id => {
     const {todosList} = this.state
-    const index = todosList.findIndex(each => {
-      if (each.id === id) {
-        return true
-      } else {
-        return false
-      }
-    })
+    const index = todosList.findIndex(each => each.id === id)
     todosList.splice(index, 1)
-    console.log(todosList)
-    this.setState({todosList: todosList})
+    this.setState({todosList})
   }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {newTodo, todosList} = this.state
+    todosList.push({id: todosList.length + 1, title: newTodo, isChecked: false})
+    this.setState({todosList})
+  }
+
+  onChangeNewtodo = event => {
+    this.setState({newTodo: event.target.value})
+  }
+
+  passEachTodo = updatedTodoObj => {
+    const {todosList} = this.state
+    const newList = todosList.map(each =>
+      each.id === updatedTodoObj.id ? updatedTodoObj : each,
+    )
+
+    this.setState({
+      todosList: newList,
+    })
+  }
+
+  passToIsChedObj = eachTitle => {
+    const {todosList} = this.state
+    const index = todosList.map(each =>
+      each.id === eachTitle.id
+        ? {...eachTitle, isChecked: !eachTitle.isChecked}
+        : each,
+    )
+    this.setState({todosList: index})
+  }
+
+  passChaningTodo = obj => {
+    // console.log(obj)
+    const {todosList} = this.state
+    const index = todosList.findIndex(each => each.id === obj.eachTitle.id)
+    // console.log(index)
+    todosList.splice(index, 1, {
+      id: obj.eachTitle.id,
+      title: obj.newtext,
+      isChecked: obj.eachTitle.isChecked,
+    })
+    // console.log(todosList)
+    this.setState({todosList})
+  }
+
   render() {
+    const {todosList} = this.state
+    // console.log(todosList)
     return (
       <div className="container">
         <ul className="ulContainer">
           <h1>Simple Todos</h1>
-          {initialTodosList.map(each => (
+          <form onSubmit={this.onSubmitForm} className="input-add-btn-center">
+            <input
+              placeholder="Input Your Todo Here"
+              className="input"
+              type="text"
+              onChange={this.onChangeNewtodo}
+            />
+            <button className="add-submit-btn ad-btn" type="submit">
+              Add
+            </button>
+          </form>
+          {todosList.map(each => (
             <TodoItem
               onDeleteuser={this.onDeleteuser}
+              passEachTodo={this.passEachTodo}
+              passToIsChedObj={this.passToIsChedObj}
+              passChaningTodo={this.passChaningTodo}
               eachTitle={each}
               key={each.id}
             />
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -3,15 +3,90 @@ import {Component} from 'react'
 import './index.css'
 
 class TodoItem extends Component {
+  state = {isEditBtnClicked: false, textToUpdate: ''}
+
+  clickToEdit = () => {
+    this.setState(prev => ({isEditBtnClicked: !prev.isEditBtnClicked}))
+  }
+
+  passChaningFunction = () => {
+    const {eachTitle, passChaningTodo} = this.props
+    const {textToUpdate} = this.state
+    passChaningTodo({eachTitle, newtext: textToUpdate})
+  }
+
+  onChangeToUpdateTodo = event => {
+    const te = event.target.value
+    this.setState({textToUpdate: te}, this.passChaningFunction)
+  }
+
+  clickToSave = () => {
+    const {eachTitle, passEachTodo} = this.props
+    const {textToUpdate} = this.state
+
+    const updatedTodoObj = {...eachTitle, title: textToUpdate}
+    passEachTodo(updatedTodoObj)
+
+    this.setState(prev => ({
+      isEditBtnClicked: !prev.isEditBtnClicked,
+      textToUpdate: '',
+    }))
+  }
+
+  isClikedInput = () => {
+    const {eachTitle, passToIsChedObj} = this.props
+    passToIsChedObj(eachTitle)
+  }
+
   render() {
     const {eachTitle, onDeleteuser} = this.props
+    const {isEditBtnClicked} = this.state
+
     const onDelete = () => {
       onDeleteuser(eachTitle.id)
     }
+
     return (
       <li className="liContainer">
-        <p>{eachTitle.title}</p>
-        <button onClick={onDelete}>Delete</button>
+        {isEditBtnClicked ? (
+          <div>
+            <input
+              id={`ch ${eachTitle.id}`}
+              className="input-to-edit-text"
+              type="text"
+              onChange={this.onChangeToUpdateTodo}
+              placeholder="Update Your Todo Here"
+              value={eachTitle.title}
+            />
+          </div>
+        ) : (
+          <div className="d-flex">
+            <input
+              className="m-2"
+              onClick={this.isClikedInput}
+              checked={eachTitle.isChecked}
+              type="checkbox"
+            />
+            <p className={`${eachTitle.isChecked && 'strike'} w-5`}>
+              {eachTitle.title}
+            </p>
+          </div>
+        )}
+
+        <div>
+          <button type="button" className="del-btn" onClick={onDelete}>
+            Delete
+          </button>
+          {isEditBtnClicked ? (
+            <button type="button" onClick={this.clickToSave}>
+              Save
+            </button>
+          ) : (
+            <button type="button" onClick={this.clickToEdit}>
+              Edit
+            </button>
+          )}
+        </div>
       </li>
     )
   }
